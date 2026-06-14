db18a198dbb2dd72d6a256aa2fd20d17b24a6cdd
DIAMOND PUBLIC SCHOOL <dpsjalesar@gmail.com>
2026-06-14 20:48:09 +0530

    commit

--- my_cryptography.py
+++ my_cryptography.py
@@ -2,9 +2,9 @@ from cryptography.fernet import Fernet
 import os
 
 def log_pin_decypt(PWD):
-    return Fernet(b'JxFCiezevkVZNmHWTFRztjSg8lREGJY5HGqtMar1Rq4=').decrypt(PWD).decode()
+    return Fernet(bytes(os.environ.get('LOGIN_PIN'))).decrypt(PWD).decode()
 def log_pin_encrypt(PWD):
-    return Fernet(b'JxFCiezevkVZNmHWTFRztjSg8lREGJY5HGqtMar1Rq4=').encrypt(PWD.encode()).decode()
+    return Fernet(bytes(os.environ.get('LOGIN_PIN'))).encrypt(PWD.encode()).decode()
 def admin_log_pass():
     return os.environ.get('ADMIN_PASSWORD')
 
